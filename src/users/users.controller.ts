952658a3feec078378035a54a928edbc12aafcad
Guilherme Silva <2003silvagui@gmail.com>
2023-09-25 17:58:50 -0300

    feat: connect

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDTO } from './dtos/create-user';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() body: CreateUserDTO) {
+    return this.usersService.createUser(body);
+  }
 
   @Get()
   async findAll() {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
+import { CreateUserDTO } from './dtos/create-user';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
+  async createUser(body: CreateUserDTO) {
+    const { email, name, password } = body;
+
+    const user = this.prisma.user.create({
+      data: {
+        email,
+        password,
+        name,
+      },
+    });
+
+    return user;
+  }
 
   async findAll() {
     return await this.prisma.user.findMany();
